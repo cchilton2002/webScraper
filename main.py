b5ffaf76dc1de5223a9ed2d68d106de21127816f
cchilton2002 <christoph0295@gmail.com>
2024-09-09 20:23:13 +0100

    Added LLM and cleaning of content scraped

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from scrape import scrapeWebsite
+from scrape import (scrapeWebsite, splitDomContent, cleanBodyContent, extractBodyContent)
+
+from parse import parseWithOllama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL")
@@ -7,4 +9,22 @@ url = st.text_input("Enter a Website URL")
 if st.button("Scrape Site"):
     st.write("Scraping Website")
     result = scrapeWebsite(url)
-    print(result)
+    bodyContent = extractBodyContent(result)
+    cleanedContent = cleanBodyContent(bodyContent)
+    
+    st.session_state.domContent = cleanedContent
+    
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleanedContent, height=300)
+    
+    
+if "domContent" in st.session_state:
+    parseDescription = st.text_area("Describe what you want to parse?")
+    if st.button("Parse Content"):
+        if parseDescription:
+            st.write("Parsing the content")
+            domChunks = splitDomContent(st.session_state.domContent)
+            result = parseWithOllama(domChunks, parseDescription)
+            st.write(result)
+            
+            
--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from bs4 import BeautifulSoup
 
 def scrapeWebsite(website):
     print("Launching chrome browser...")
@@ -10,10 +11,35 @@ def scrapeWebsite(website):
     driver = webdriver.Chrome(service=Service(chromeDriverPath),options=options)
     
     try:
-        driver.get(website)
+        #driver.get(website)
         print("Page loaded...")
         html = driver.page_source
         time.sleep(10)
         return html
     finally:
         driver.quit()
+        
+def extractBodyContent(htmlContent):
+    soup = BeautifulSoup(htmlContent, "html.parser")
+    bodyContent = soup.body
+    if bodyContent:
+        return str(bodyContent)
+    return ""
+
+def cleanBodyContent(bodyContent):
+    soup = BeautifulSoup(bodyContent, "html.parser")
+    
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+    
+    cleanedContent = soup.get_text(separator="\n")
+    cleanedContent = "\n".join(
+        line.strip() for line in cleanedContent.splitlines() if line.strip()
+    )
+    
+    return cleanedContent
+
+def splitDomContent(domContent, maxLength=6000):
+    return [
+        domContent[i:i+maxLength] for i in range(0, len(domContent),maxLength)
+    ]
